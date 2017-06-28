a27def9e425fbb0f08af96cc25845efa8a2d8820
Don Carpenter <dekc@outlook.com>
2017-06-28 22:10:10 +1000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import util from 'util';
+// const util = require('util');
+
+const field1 = 'key1';
+const field2 = 'key2';
+
+const obj = {
+  [field1]: 'value1',
+  [field2]: 'value2'
+};
+
+console.dir(obj);
+console.log('Done');
